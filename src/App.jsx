6c5c2626ea8fd5ebf68918c72f329c1e7fb7b8a8
James Greaves <jgreavesei@gmail.com>
2024-02-08 16:27:26 +0000

    Added dom router and remvoed top component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/";
 import MapComponent from "./components/MapComponent/";
 import ReportForm from "./components/ReportForm/";
 import Footer from "./components/Footer/";
+import PrivacyNotice from "./components/PrivacyNotice";
+import TermsOfService from "./components/TermsOfService/";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <MapComponent />
-      <ReportForm />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" component={MapComponent} />
+          <Route path="/report" component={ReportForm} />
+          <Route path="/privacy" component={PrivacyNotice} />
+          <Route path="/terms" component={TermsOfService} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
