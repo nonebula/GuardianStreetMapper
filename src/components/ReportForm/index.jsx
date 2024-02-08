85321c06d9c6a645909b6043d4066667817e6764
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-02-08 15:38:00 +0000

    Content added for tips & info

--- src/components/ReportForm/index.jsx
+++ src/components/ReportForm/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from "./ReportForm.module.css"
+import style from "./style.css"
 
 function ReportForm() {
   // Logic for handling form submission and validation can be added here
--- src/components/TipsAndInformation/index.jsx
+++ src/components/TipsAndInformation/index.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const TipsAndInfo = () => {
-    return (
-        <div>
-        <h1>Tips & Information</h1>
-        <p>This is the Tips page.</p>
-      </div>
-    );
+  return (
+    <div>
+      <h1>Tips & Information</h1>
+      <p>iPhone: Turn on 'Stolen Device Protection'</p>
+      <h2>If you've been stolen from, follow these steps now:</h2>
+      <ol>
+        <li>
+          iPhone Users - Report device stolen through your AppleID, put into
+          lost mode.
+        </li>
+        <li>Secure all sensitive information, passwords and banks.</li>
+        <li>Call the Police</li>
+        <li>Deactivate social media and contact friends and family</li>
+        <li>Contact credit reference agencies</li>
+      </ol>
+    </div>
+  );
 };
 
 export default TipsAndInfo;
