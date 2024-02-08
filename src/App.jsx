ce59b3ecfca066fb52caafb4032a4ff3a4496434
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-02-08 14:21:03 +0000

    CAPTCHA v3 basic setup & code. No POST request coding yet.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Header from './components/Header/Header';
 import MapComponent from './components/MapComponent/MapComponent';
 import ReportForm from './components/ReportForm/ReportForm';
 import Footer from './components/Footer/Footer';
+import Captcha from './components/Captcha';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Captcha />
       <MapComponent />
       <ReportForm />
       <Footer />
