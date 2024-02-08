ba30822865324bc2d2f86c05f86952df283d88e4
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-02-08 19:12:02 +0000

    Jim's finished Router

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import PrivacyNotice from '../PrivacyNotice';
-import TermsOfService from '../TermsOfService';
+import React from "react";
+import PrivacyNotice from "../PrivacyNotice";
+import TermsOfService from "../TermsOfService";
+import { Link } from "react-router-dom";
 
 // import style from "./style.css"
-
 function Footer() {
   return (
     <footer>
-      <p>&copy; 2024 Stolen Items Reporting App</p>
       <div className="footer-buttons">
-        <button onClick={() => window.alert('<PrivacyNotice />')}>Privacy Notice</button>
-        <button onClick={() => window.alert('Terms of Service')}>Terms of Service</button>
+        <Link to="/privacy">
+          <button>Privacy Notice</button>
+        </Link>
+        <Link to="/terms">
+          <button>Terms of Service</button>
+        </Link>
+        <p>&copy; 2024 Stolen Items Reporting App</p>
       </div>
     </footer>
   );
 }
-
 export default Footer;
