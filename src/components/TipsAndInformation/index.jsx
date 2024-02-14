c8008eb3f5eaab700b61b6375789a3d38d016286
Mac <mac@Macs-MacBook-Air.local>
2024-02-14 13:20:32 +0000

    added more to tips&info

--- src/components/TipsAndInformation/index.jsx
+++ src/components/TipsAndInformation/index.jsx
@@ -15,15 +15,39 @@ const TipsAndInfo = () => {
       When using the Guardian Street Mapper App, follow these best practices to enhance the accuracy and effectiveness of your incident reports:
       <li><b>Location Accuracy:</b> Pinpoint the exact location of the incident on the map for precise reporting.
         </li>
+
         <li><b>Detailed Descriptions:</b> Provide clear and concise details about the incident, including the type of crime, location, date, and time.
         </li>
 
         <li><b>Media Attachments:</b> Whenever possible, include photos or videos related to the incident to provide additional context.
         </li>
+
         <li><b>Witness Information:</b> If applicable, include information about any witnesses to the crime.
         </li>
+
         <li><b>Contact Information:</b> Provide your contact information in case authorities need to follow up on the report.
         </li>
+      </ol>
+
+      <h2 className={styles.heading}>Safety Tips</h2>
+      <ol className={styles.paragraph}>
+        Help promote personal and community safety by following these general safety tips:
+        <li><b>Stay Informed:</b> Stay updated on the city and comunity crime trends and safety measures to better protect yourself and your community.
+        </li>
+
+        <li><b>Report Suspicious Activity:</b> If you witness suspicious behavior, report it promptly through the app or contact local law enforcement.
+        </li>
+
+        <li><b>Home Security:</b> Implement basic home security measures, such as ensuring proper lighting and secure locks.
+        </li>
+
+        <li><b>Emergency Contacts:</b> Keep emergency contact numbers readily available for quick access.
+        </li>
+
+        <li><b>Community Watch Programs:</b> Participate in or support community watch programs to collectively enhance community safety.
+        </li>
+
+
       </ol>
     </div>
   );
