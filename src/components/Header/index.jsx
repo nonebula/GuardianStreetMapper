0ca8b8738e9e3d91212b4bb66658ef57187a1eaf
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-02-14 22:02:35 +0000

    Linking pages

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,18 +6,26 @@ function Header() {
   return (
     <header className={styles.heading}>
       <Navbar className={styles.navbar} expand="lg">
-          <Navbar.Brand className={styles.logo} href="/">
-            <img src={logo} alt="Application Logo" width="150" height="50" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className={styles.navLinks}>
-              <Nav.Link className={styles.navLink} href="/about">About</Nav.Link>
-              <Nav.Link className={styles.navLink} href="/tips">Tips & Information</Nav.Link>
-              <Nav.Link className={styles.navLink} href="/support">Support</Nav.Link>
-              <Nav.Link className={styles.navLink} href="/contact">Contact Us</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+        <Navbar.Brand className={styles.logo} href="/">
+          <img src={logo} alt="Application Logo" width="150" height="50" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className={styles.navLinks}>
+            <Nav.Link className={styles.navLink} as={Link} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link className={styles.navLink} as={Link} to="/tips">
+              Tips & Information
+            </Nav.Link>
+            <Nav.Link className={styles.navLink} as={Link} to="/support">
+              Support
+            </Nav.Link>
+            <Nav.Link className={styles.navLink} as={Link} to="/contact">
+              Contact Us
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </header>
   );
