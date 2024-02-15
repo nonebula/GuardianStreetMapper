d70a326c836f4709354d742d6bf2336c231a1d33
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-02-15 15:24:45 +0000

    Captcha

--- src/components/Captcha/index.jsx
+++ src/components/Captcha/index.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 
+import axios from "axios";
 import styles from "./Captcha.module.css";
 
 const Captcha = () => {
-  const [captchaToken, setCaptchaToken] = useState('');
+  const [captchaToken, setCaptchaToken] = useState("");
 
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
-      "https://www.google.com/recaptcha/api.js?render=6Lfx9GspAAAAAKlMVg6QmlZakk_MqNqt-UAgbTd-"; 
+      "https://www.google.com/recaptcha/api.js?render=6Lfx9GspAAAAAKlMVg6QmlZakk_MqNqt-UAgbTd-";
     script.async = true;
     document.body.appendChild(script);
 
@@ -25,29 +26,36 @@ const Captcha = () => {
   const executeCaptcha = () => {
     window.grecaptcha.ready(() => {
       window.grecaptcha
-        .execute("6Lfx9GspAAAAAKlMVg6QmlZakk_MqNqt-UAgbTd-", { action: "submit" }) 
+        .execute("6Lfx9GspAAAAAKlMVg6QmlZakk_MqNqt-UAgbTd-", {
+          action: "submit",
+        })
         .then(handleCaptchaChange);
     });
   };
 
-  // const handleSubmit = async () => {
-  //   try {
-  //     const response = await axios.post('/verify-recaptcha', { token: captchaToken });
-  //     const { success } = response.data;
-  //     if (success) {
-  //       console.log('reCAPTCHA verified, submitting form...');
-  //       triggerFormSubmission(); 
-  //     } else {
-  //       console.log('reCAPTCHA verification failed');
-  //     }
-  //   } catch (error) {
-  //     console.error('Error verifying reCAPTCHA:', error);
-  //   }
-  // };
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post("/verify-recaptcha", {
+        token: captchaToken,
+      });
+      const { success } = response.data;
+      if (success) {
+        console.log("reCAPTCHA verified, submitting form...");
+        triggerFormSubmission();
+      } else {
+        console.log("reCAPTCHA verification failed");
+      }
+    } catch (error) {
+      console.error("Error verifying reCAPTCHA:", error);
+    }
+  };
+  console.log(handleSubmit);
 
   return (
     <div>
-      <button className={styles.submitButton} onClick={executeCaptcha}>Submit Incident</button>
+      <button className={styles.submitButton} onClick={executeCaptcha}>
+        Submit Incident
+      </button>
     </div>
   );
 };
